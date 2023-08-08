f37fa722db8d3ca669d76771a486f1f326296146
Alexander Kühhas <alexander.kuehhas@tttech-auto.com>
2023-08-08 15:32:28 +0200

    NOISSUE-1 add new functions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog, Scale, Canvas, Scrollbar, Label
+from tkinter import filedialog, Scale, Canvas, Scrollbar, Label, Button, Frame
 from PIL import Image, ImageTk
 
 
@@ -7,11 +7,13 @@ class ImageApp:
 
     def __init__(self, master):
         self.master = master
-        self.master.title("Image editing")
+        self.master.title("Image Editor")
         self.master.geometry('1000x800')
 
         self.original_image = None
         self.display_image = None
+        self.scaled_width = None
+        self.scaled_height = None
 
         # Labels for displaying image dimensions
         self.original_dims_label = Label(self.master, text="Original: - x -")
@@ -19,6 +21,15 @@ class ImageApp:
 
         self.scaled_dims_label = Label(self.master, text="Scaled: - x -")
         self.scaled_dims_label.pack(pady=10)
+
+        self.copy_controls_frame = Frame(self.master)
+        self.copy_controls_frame.pack(pady=10)
+
+        self.copy_width_button = Button(self.copy_controls_frame, text="Copy Width", command=self.copy_width)
+        self.copy_width_button.pack(side=tk.LEFT, padx=5)
+
+        self.copy_height_button = Button(self.copy_controls_frame, text="Copy Height", command=self.copy_height)
+        self.copy_height_button.pack(side=tk.LEFT, padx=5)
 
         self.canvas_frame = tk.Frame(self.master)
         self.canvas_frame.pack(fill=tk.BOTH, expand=tk.YES, padx=20, pady=20)
@@ -35,12 +46,22 @@ class ImageApp:
         self.canvas.configure(yscrollcommand=self.v_scroll.set, xscrollcommand=self.h_scroll.set)
         self.canvas.bind("<Configure>", self.on_canvas_configure)
 
-        self.load_button = tk.Button(self.master, text="Load image", command=self.load_image)
+        self.load_button = tk.Button(self.master, text="Load Image", command=self.load_image)
         self.load_button.pack(pady=10)
 
-        self.slider = Scale(self.master, from_=1, to=300, orient="horizontal", label="Image size in %",
-                            command=self.update_image_size)
-        self.slider.pack(pady=10)
+        # Adjust the width of the slider and add buttons for scaling
+        self.zoom_control_frame = Frame(self.master)
+        self.zoom_control_frame.pack(pady=10)
+
+        self.zoom_out_button = Button(self.zoom_control_frame, text="Zoom Out", command=self.zoom_out)
+        self.zoom_out_button.pack(side=tk.LEFT, padx=5)
+
+        self.slider = Scale(self.zoom_control_frame, from_=1, to=300, orient="horizontal", label="Image Size in %",
+                            length=500, command=self.update_image_size)
+        self.slider.pack(side=tk.LEFT, padx=10)
+
+        self.zoom_in_button = Button(self.zoom_control_frame, text="Zoom In", command=self.zoom_in)
+        self.zoom_in_button.pack(side=tk.LEFT, padx=5)
 
     def reset_controls(self):
         self.slider.set(100)
@@ -66,6 +87,8 @@ class ImageApp:
             scale_factor = float(scale_value) / 100.0
             new_width = int(self.original_image.width * scale_factor)
             new_height = int(self.original_image.height * scale_factor)
+            self.scaled_width = new_width
+            self.scaled_height = new_height
             self.display_image = self.original_image.resize((new_width, new_height))
 
             # Update the scaled dimensions label
@@ -79,6 +102,24 @@ class ImageApp:
             self.canvas.create_image(0, 0, anchor=tk.NW, image=tk_image)
             self.canvas.image = tk_image
             self.canvas.config(scrollregion=self.canvas.bbox("all"))
+
+    def zoom_in(self):
+        new_value = min(300, self.slider.get() + 1)  # Increment by 10% but limit to 300%
+        self.slider.set(new_value)
+        self.update_image_size(new_value)
+
+    def zoom_out(self):
+        new_value = max(1, self.slider.get() - 1)  # Decrement by 10% but don't go below 1%
+        self.slider.set(new_value)
+        self.update_image_size(new_value)
+
+    def copy_width(self):
+        self.master.clipboard_clear()
+        self.master.clipboard_append(self.scaled_width)
+
+    def copy_height(self):
+        self.master.clipboard_clear()
+        self.master.clipboard_append(self.scaled_height)
 
 
 if __name__ == "__main__":
